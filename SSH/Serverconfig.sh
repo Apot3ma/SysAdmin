8bac2a3468740e1e04122be73a4e3515ab1929ec
Apot3ma <filiberogalvez@gmail.com>
2025-05-06 16:43:43 -0700

    libreria para ssh de linux

--- SSH/Serverconfig.sh
+++ SSH/Serverconfig.sh
@@ -1,8 +1,15 @@
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install openssh-server -y
-sudo systemctl restart ssh
-systemctl status ssh
+#!/bin/bash
+source ./ssh/Lib.sh
 
-echo "Servidor establecido, ingrese en esta ip en el cliente"
-hostname -I
+
+echo "Bienvenido al menu de configuracion del servidor ssh"
+echo "1. instalacion de ssh"
+echo "2. estado del servidor"
+echo "3. reiniciar el servidor"
+echo "4. ver ip"
+echo "5. crear usuario"
+echo "6. ver usuarios"
+echo "7. eliminar usuario"
+echo "8. salir"
+read -p "ingrese la opcion deseada: " opcion
+
